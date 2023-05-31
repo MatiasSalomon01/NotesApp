681f8fdde6228b9698a6ba989f80a87d6861191c
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-31 09:44:55 -0400

    Agregando barra lateral separador en TodoView, pero todavia no termina donde termina la info

--- lib/views/todo_view.dart
+++ lib/views/todo_view.dart
@@ -8,28 +8,56 @@ class TodoView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        // color: Colors.red,
-        margin: const EdgeInsets.only(top: 5, bottom: 15, left: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Creado el: ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}",
-              style: const TextStyle(fontSize: 15, color: Colors.grey),
-            ),
-            Expanded(
-              child: ListView.separated(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: Listas.listas.length,
-                itemBuilder: (context, index) => TodoItem(
-                    description: Listas.listas[index].descripcion!,
-                    isCompleted: Listas.listas[index].isCompleted!),
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(),
+        margin: const EdgeInsets.only(top: 8, bottom: 15, left: 5),
+        child: Container(
+          child: Row(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    color: Colors.grey.withOpacity(0.3),
+                    width: 2,
+                    margin: const EdgeInsets.symmetric(horizontal: 12),
+                  ),
+                  Positioned(
+                    right: 2,
+                    child: Container(
+                      color: const Color(0xffF3F3F3),
+                      child: Icon(
+                        Icons.commit_outlined,
+                        size: 22,
+                        color: Colors.black26,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      " Creado el: ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}",
+                      style: const TextStyle(fontSize: 15, color: Colors.grey),
+                    ),
+                    const SizedBox(height: 3),
+                    Expanded(
+                      child: ListView.separated(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount: Listas.listas.length,
+                        itemBuilder: (context, index) => TodoItem(
+                            description: Listas.listas[index].descripcion!,
+                            isCompleted: Listas.listas[index].isCompleted!),
+                        separatorBuilder: (BuildContext context, int index) =>
+                            const Divider(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
