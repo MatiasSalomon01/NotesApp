eae5000ec0a3aef41a1a95d5f03e7f6b78495d8a
=Matias Salomon <matias.salomon@clt.com.py>
2023-06-02 16:51:05 -0400

    Creacion e Implementacion de Modal en CustomTextButton

--- lib/layouts/widgets/custom_text_button.dart
+++ lib/layouts/widgets/custom_text_button.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class CustomTextButton extends StatelessWidget {
-  const CustomTextButton({super.key});
+class CustomTextButton extends StatefulWidget {
+  CustomTextButton({super.key});
+
+  @override
+  State<CustomTextButton> createState() => _CustomTextButtonState();
+}
+
+class _CustomTextButtonState extends State<CustomTextButton> {
+  TextEditingController controllerTitle = TextEditingController();
+  TextEditingController controllerDescription = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +21,86 @@ class CustomTextButton extends StatelessWidget {
           overlayColor: MaterialStateProperty.all(Colors.grey[400]),
         ),
         onPressed: () {
-          print(MediaQuery.of(context).size);
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Creacion de Tarea'),
+                content: Container(
+                  height: 150,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        enabled: false,
+                        cursorColor: Colors.black,
+                        decoration: InputDecoration(
+                          hintText:
+                              '${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}',
+                          focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: controllerTitle,
+                        cursorColor: Colors.black,
+                        decoration: const InputDecoration(
+                          hintText: 'Titulo',
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: controllerDescription,
+                        cursorColor: Colors.black,
+                        decoration: const InputDecoration(
+                          hintText: 'Descripcion',
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    style: ButtonStyle(
+                      overlayColor: MaterialStateProperty.all(Colors.grey[400]),
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.grey[300]),
+                    ),
+                    onPressed: () {
+                      if (controllerTitle.text.isEmpty ||
+                          controllerDescription.text.isEmpty) {
+                        print("ERROR LOS INPUTS ESTAN VACIOS");
+                      } else {
+                        print(
+                            "INFORMACION: ${controllerTitle.text} - ${controllerDescription.text}");
+                      }
+                    },
+                    child: const Text('Crear',
+                        style: TextStyle(color: Colors.black, fontSize: 15)),
+                  ),
+                  TextButton(
+                    style: ButtonStyle(
+                      overlayColor: MaterialStateProperty.all(Colors.grey[400]),
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.grey[300]),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      'Cancelar',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  )
+                ],
+              );
+            },
+          );
         },
         child: Row(
           children: const [
